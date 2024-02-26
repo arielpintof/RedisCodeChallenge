ed7d8b3d21cd285ad45906d8ffd325aa72c2462f
Ariel Pinto <ariel.pintof@userena.cl>
2024-02-26 02:55:27 -0300

    pass 4rd  stage

--- src/Server.cs
+++ src/Server.cs
@@ -5,24 +5,28 @@ using System.Text;
 
 Console.WriteLine("Logs from your program will appear here!");
 
-const string response = "+PONG\r\n";
+var response = Encoding.UTF8.GetBytes("+PONG\r\n");
 
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 
-await Task.Run(async () =>
+
+while (true)
 {
-    while (true)
+    var client = await server.AcceptTcpClientAsync();
+
+    await Task.Run(async () =>
     {
-        var socket = await server.AcceptSocketAsync();
         var buffer = new byte[1024];
-        var received = await socket.ReceiveAsync(buffer, SocketFlags.None);
-        var msg = Encoding.ASCII.GetString(buffer, 0, received);
-        if (msg.Length == 0) break;
-    
-        await socket.SendAsync(Encoding.UTF8.GetBytes(response), SocketFlags.None);
-    }
-});
+        var stream = client.GetStream();
+        var received = stream.Read(buffer, 0, buffer.Length);
+        while (received > 0)
+        {
+            var _ = Encoding.UTF8.GetString(buffer);
+            await stream.WriteAsync(response);
+            received = stream.Read(buffer, 0, buffer.Length);
+        }
 
 
-
+    });
+}
