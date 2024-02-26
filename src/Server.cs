bd4533df23394dc92c2303276d17455219fbe0ee
Ariel Pinto <ariel.pintof@userena.cl>
2024-02-26 02:39:11 -0300

    pass 4rd  stage

--- src/Server.cs
+++ src/Server.cs
@@ -11,13 +11,18 @@ var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 var socket = await server.AcceptSocketAsync();
 
-while (true)
+await Task.Run(async () =>
 {
-    var buffer = new byte[1024];
-    var received = await socket.ReceiveAsync(buffer, SocketFlags.None);
-    var msg = Encoding.ASCII.GetString(buffer, 0, received);
-    if (msg.Length == 0) break;
+    while (true)
+    {
+        var buffer = new byte[1024];
+        var received = await socket.ReceiveAsync(buffer, SocketFlags.None);
+        var msg = Encoding.ASCII.GetString(buffer, 0, received);
+        if (msg.Length == 0) break;
     
-    await socket.SendAsync(Encoding.UTF8.GetBytes(response), SocketFlags.None);
-}
+        await socket.SendAsync(Encoding.UTF8.GetBytes(response), SocketFlags.None);
+    }
+});
+
+
 
