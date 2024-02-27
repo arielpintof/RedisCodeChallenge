f9d161090d2bbf0dcaed66e471a1814085b5531d
Ariel Pinto <ariel.pintof@userena.cl>
2024-02-27 19:52:07 -0300

    replication 2

--- src/RespExpression.cs
+++ src/RespExpression.cs
@@ -39,7 +39,7 @@ public class RespExpression
 
     private string HandleInfoCommand()
     {
-        return _value[4].Equals("replication") ? "role:master" : Resp.NullEncode();
+        return _value[4].Equals("replication") ? Resp.BulkEncode("role:master") : Resp.NullEncode();
     }
 
     private string HandleSetCommand(Store store)
