ea80a145c2b3bbef2284213e313461b2e2f0b71c
Ariel Pinto <ariel.pintof@userena.cl>
2024-02-27 17:14:52 -0300

    pass 7th stage

--- src/Store.cs
+++ src/Store.cs
@@ -1,11 +1,13 @@
-﻿namespace codecrafters_redis;
+﻿using System.Collections.Concurrent;
+
+namespace codecrafters_redis;
 
 
 //Clave valor
 //Operaciones Set y Get
 public class Store
 {
-    private readonly Dictionary<string, Data> _storeValues = new();
+    private readonly ConcurrentDictionary<string, Data> _storeValues = new();
 
     public bool Set(string key, string value, int expiration = 0) => 
         _storeValues.TryAdd(key, new Data(value, expiration));
@@ -20,11 +22,11 @@ public class Store
             return value.Value;
         }
 
-        _storeValues.Remove(key, out _);
+        _storeValues.TryRemove(key, out _);
 
         return null;
 
     } 
-    public bool Remove(string key) => _storeValues.Remove(key);
+    
 
 }
