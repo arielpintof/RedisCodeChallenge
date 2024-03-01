c252659ef6560e803bcbb28c3b4b34b8ae0676ef
Ariel Pinto <ariel.pintof@userena.cl>
2024-03-01 16:19:30 -0300

    replication master handshake response

--- src/RespExpression.cs
+++ src/RespExpression.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace codecrafters_redis;
+﻿namespace codecrafters_redis;
 
 public class RespExpression
 {
@@ -10,7 +8,7 @@ public class RespExpression
     {
         Value = value.ToList();
     }
-    
+
     private Command Command => Value[2].ToLowerInvariant() switch
     {
         "ping" => Command.Ping,
@@ -23,20 +21,18 @@ public class RespExpression
         _ => throw new ArgumentOutOfRangeException()
     };
 
-    public IEnumerable<string> GetMessage(Store store) => new[]
-    {
+    public IEnumerable<string> GetMessage(Store store) =>
         Command switch
         {
             Command.Ping => HandlePingCommand(),
-            Command.Echo => Resp.BulkEncode(Value[4]),
+            Command.Echo => new List<string>{Resp.BulkEncode(Value[4])},
             Command.Set => HandleSetCommand(store),
             Command.Get => HandleGetCommand(store),
             Command.Info => HandleInfoCommand(),
             Command.Replconf => HandleReplCommand(),
             Command.Psync => HandlePsyncCommand(),
             _ => throw new ArgumentOutOfRangeException()
-        }
-    };
+        };
 
     private IEnumerable<string> HandlePsyncCommand()
     {
@@ -48,47 +44,51 @@ public class RespExpression
         };
     }
 
-    private string HandleReplCommand()
+    private IEnumerable<string> HandleReplCommand()
     {
-        return Resp.SimpleEncode("OK");
+        return new List<string>
+        {
+            Resp.SimpleEncode("OK")
+        };
     }
 
-    private string HandlePingCommand()
+    private IEnumerable<string> HandlePingCommand()
     {
-        return Resp.SimpleEncode("PONG");
+        return new List<string>
+            { Resp.SimpleEncode("PONG") };
     }
 
-    private string HandleInfoCommand()
+    private IEnumerable<string> HandleInfoCommand()
     {
         if (!this.ValueHas("info") && !this.ValueHas("replication"))
-            return Resp.BulkEncode($"role:{ServerSettings.Role}");
-        
+            return new List<string> { Resp.BulkEncode($"role:{ServerSettings.Role}") };
+
         var response = new List<string>
         {
             $"role:{ServerSettings.Role}",
             $"master_replid:{ServerSettings.MasterId}",
             "master_repl_offset:0"
         };
-            
-        return Resp.BulkEncode(string.Join(Resp.Separator, response));
+
+        return new List<string> { Resp.BulkEncode(string.Join(Resp.Separator, response)) };
     }
 
-   
-    private string HandleSetCommand(Store store)
+
+    private IEnumerable<string> HandleSetCommand(Store store)
     {
-        var expiration = TryGetArgument("px", out var expirationValue) 
+        var expiration = TryGetArgument("px", out var expirationValue)
             ? int.Parse(expirationValue)
             : 0;
-        
-        return store.Set(GetKey, GetValue, expiration) ? Resp.SimpleEncode("OK") : "null";
+
+        return new List<string> { store.Set(GetKey, GetValue, expiration) ? Resp.SimpleEncode("OK") : "null" };
     }
 
-    private string HandleGetCommand(Store store)
+    private IEnumerable<string> HandleGetCommand(Store store)
     {
         var value = store.GetValue(GetKey);
-        return value!.Length > 0 ? Resp.BulkEncode(value) : Resp.NullEncode();
+        return new List<string> { value!.Length > 0 ? Resp.BulkEncode(value) : Resp.NullEncode() };
     }
-    
+
     private string GetKey => Value[4];
     private string GetValue => Value[6];
 
@@ -104,7 +104,6 @@ public class RespExpression
         value = $"{index}";
         return true;
     }
-
 }
 
 public static class RespExtension
@@ -113,7 +112,4 @@ public static class RespExtension
     {
         return expression.Value.Any(e => e.Equals(argument));
     }
-
-    
-    
 }
